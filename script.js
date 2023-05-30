fbca74d9a0fcc1edf39ee47d0c282224394757b5
gabriel <gabmt2210@gmail.com>
2023-05-30 20:29:00 -0300

    dark mode

---
+++ script.js
@@ -0,0 +1,6 @@
+function trocar(cor){
+    document.getElementById("corpo").style.backgroundColor = cor
+    document.getElementById("darkmode").classList.toggle("desligado")
+    document.getElementById("darkmode2").classList.toggle("desligado")
+    
+}
