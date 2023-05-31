50a1b25871050728962955fc5ad9320603263274
silvia <silviakavabata@gmail.com>
2023-05-30 23:09:33 -0300

    home pronta

--- script.js
+++
@@ -1,6 +0,0 @@
-function trocar(cor){
-    document.getElementById("corpo").style.backgroundColor = cor
-    document.getElementById("darkmode").classList.toggle("desligado")
-    document.getElementById("darkmode2").classList.toggle("desligado")
-    
-}
